6530c4ac94351915ce04bc54aa85673e54ed9ff8
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-07-22 06:27:43 -0500

    updated

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-//Call Hello World 
-function helloWorld(){
-    alert("hello World");
+//Get the string from the page
+//cotroller function
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
 }
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+    let revString = [];
+    //reverse a string using a for loop
+    //the value of the last index of an array equals length -1
+    for (let index = userString.length-1; index >= 0; index--){
+            revString += userString[index];     
+    }
+    return revString;
+}
+
+//Display the reversed string
+//view function
+function displayString(revString){
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+} 
